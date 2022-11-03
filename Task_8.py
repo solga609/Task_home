d5be05a18747db703ec378c5259a811b310a3698
Olga <olga609@mail.ru>
2022-11-03 13:21:23 +0200

    first_task

--- Task_8.py
+++ Task_8.py
@@ -23,3 +23,44 @@ while True:
     else:
         print("Неверный знак операции!")
 
+
+#  2. Создать телефонный справочник с возможностью импорта и экспорта данных в нескольких форматах.
+
+import pickle
+class Book():
+    def __init__(self):
+        self.Dict = {}
+    def enter(self):
+    
+        while True:
+            self.name_family = input("Введите имя и фамилию человека: ")
+            self.nomer = input("Введите его номер телефона: ")
+            self.Dict [self.name_family] = self.nomer
+            self.file = open("Text.bin", 'wb')
+            pickle.dump(self.Dict, self.file) 
+            self.file.close()
+ 
+            exit_menu = input("Добавить ещё данные или выйти в меню?")
+            if exit_menu == "Выйти" or exit_menu == "выйти":
+                break
+    def output(self):
+        file = open('Text.bin', 'rb')
+        self.Dict = pickle.load(file)
+        print(self.Dict)
+ 
+    def Change(self):
+        self.output()
+        self.name_family_change = input("Введите имя и фамилию человека чей номер хотите изменить или удалить:\n ")
+        if self.name_family_change in self.Dict:
+            self.del_and_change = input("Удалить или изменить? \n")
+        else:
+            print("Такого человека нету в списке попробуйте снова")
+ 
+ 
+ 
+a = Book()
+a.enter()
+a.output()
+a.Change()
+
+
